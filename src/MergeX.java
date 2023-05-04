2ee793f3921d2e20011523446fe6e8d633130174
AnkitKumar <ankititmuniversity@gmail.com>
2023-05-04 20:54:26 +0530

    MergeX

---
+++ src/MergeX.java
@@ -0,0 +1,12 @@
+
+public class MergeX {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi X1");
+		System.out.println("Hi X2");
+		System.out.println("Hi X3");
+		System.out.println("Hi X4");
+	}
+
+}
